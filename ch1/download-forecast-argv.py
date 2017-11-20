bd7804966cc433637d4a4e366f3b13487e00f1d7
minsik <yms9654@gmail.com>
2017-11-20 10:51:34 +0900

    update

--- ch1/download-forecast-argv.py
+++ ch1/download-forecast-argv.py
@@ -4,17 +4,19 @@ import urllib.parse as parse
 
 if len(sys.argv) <= 1:
     print("USAGE: download-forecast-argv <Region Number>")
+    sys.exit()
+
+regionNumber = sys.argv[1]
 
 API = "http://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp"
-
 values = {
-    'stnId': '109'
+    'stnId': regionNumber
 }
 
-params = urllib.parse.urlencode(values)
+params = parse.urlencode(values)
 url = API + "?" + params
 print("url=", url)
 
-data = urllib.request.urlopen(url).read()
+data = req.urlopen(url).read()
 text = data.decode('utf-8')
 print(text)
